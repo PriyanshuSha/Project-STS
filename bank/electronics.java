3c2293edbc73b3989cce4db5c56b40cb4497f75a
PriyanshuSha <109303639+PriyanshuSha@users.noreply.github.com>
2022-09-01 23:14:52 +0530

    Add files via upload

---
+++ bank/electronics.java
@@ -0,0 +1,21 @@
+package first.bank;
+
+public class electronics {
+	 // These all are instance variable
+	int eId;       
+	String eName;
+    String eCompany;
+    int ePrice;
+    String eModel;
+	public electronics(int eId, String eame, String eCompany, int ePrice, String eModel) {
+		
+		this.eId = eId;
+		this.eName = eame;
+		this.eCompany = eCompany;
+		this.ePrice = ePrice;
+		this.eModel = eModel;
+	}
+	
+    
+	
+}
