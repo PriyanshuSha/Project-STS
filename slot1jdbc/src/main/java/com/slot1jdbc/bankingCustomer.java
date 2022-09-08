ac6394ebe9ad14002915dc6379da5d886c79176c
PriyanshuSha <109303639+PriyanshuSha@users.noreply.github.com>
2022-09-08 15:06:50 +0530

    Add files via upload

---
+++ slot1jdbc/src/main/java/com/slot1jdbc/bankingCustomer.java
@@ -0,0 +1,9 @@
+package com.slot1jdbc;
+                        
+public class bankingCustomer {
+	String cName;       // Here cName is use as a customer name 
+	String cPassword;   // Here cPassword is use as a customer Password
+	String cPhone;      // Here cPhone is use as a customer phone number
+	int cAccbal;        // Here cAccbal is use as a customer account balance
+
+}
